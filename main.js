6ee2ab503c9fa272086c0f3aaec835463aa7c389
Homer <h0mer13@proton.me>
2023-01-13 16:10:22 +0100

    plenty changes ;)

--- main.js
+++ main.js
@@ -1,29 +1,39 @@
-const backgroundEl = document.getElementById("bg-img");
-const aboutmeEl = document.getElementById("aboutme");
-const mydreamsEl = document.getElementById("mydreams");
-const myprojectsEl = document.getElementById("myprojects");
+const backgroundEl = document.body;
+const aboutme = document.getElementById("aboutme");
+const skills = document.getElementById("skills");
+const contact = document.getElementById("contact");
+const hamburger = document.querySelector(".hamburger");
+const container = document.querySelector(".container")
+let audio = new Audio("./assets/audio/babel.mp3");
+audio.loop = true;
+
+
 
 window.addEventListener("scroll", () => {
-    backgroundEl.style.opacity = 1 - window.scrollY*0.001;
-    backgroundEl.style.backgroundSize = 120 + window.scrollY*0.1 +"%";
-    // aboutmeEl.style.left = 2000 - window.scrollY*2.5 + "px";
-   if (window.scrollY > 200) {
-    aboutmeEl.style.setProperty("left", 150+"px");
-    mydreamsEl.style.setProperty("left", 500+"px");
-    myprojectsEl.style.setProperty("left", 850+"px");
-   } else {
-     aboutmeEl.style.setProperty("left", 2000+"px");
-     mydreamsEl.style.setProperty("left", 3000+"px");
-     myprojectsEl.style.setProperty("left", 4000+"px");
+   
+    backgroundEl.style.backgroundSize = 120 + window.scrollY*0.2 +"%";
+    
+   if (window.scrollY > 210)  {
+    audio.play();
+    aboutme.style.setProperty("left", 8+"%");
+    skills.style.setProperty("left", 38+"%");
+    contact.style.setProperty("left", 68+"%");
+   } 
+   else {
+    audio.pause();
+     aboutme.style.setProperty("left", 2000+"px");
+     skills.style.setProperty("left", 3000+"px");
+     contact.style.setProperty("left", 4000+"px");
     }
 
 })
-aboutmeEl.addEventListener("mouseover", () => {
-    aboutmeEl.innerHTML = `
-   This 43 year old guy is a passionate individual who loves to stay active and up to date with the latest technology. He is a fan of Bitcoin and inline skating, and he is looking to further his knowledge and skills in web development. After 15 years working on sea as Officer of the watch now is changing his carrier. He is a hardworking, motivated individual who is eager to learn and take on new challenges. He is an organized and detail-oriented person who is committed to delivering quality results. Bitcoin class 2013.
-    `
+
+
+
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    container.classList.toggle("active");
 })
-aboutmeEl.addEventListener("mouseleave", () => {
-   
-    aboutmeEl.innerHTML = `about me`
-})
+
+
